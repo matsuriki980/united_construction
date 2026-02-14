c9393caa24353073aaaf013ecb5782488f781dc1
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-02-14 10:21:26 +0900

    2/13 作業内容 コミット

---
+++ static/js/component/hamburger-menu.js
@@ -0,0 +1,18 @@
+/**
+ * ハンバーガーメニュー
+ */
+export const initializeHamburgerMenu = () => {
+  const menu = document.querySelector(".js-header-menu");
+  const openButton = document.querySelector(".js-header-menu-open-button");
+  const closeButton = document.querySelector(".js-header-menu-close-button");
+
+  if (!menu || !closeButton || !openButton) return;
+
+  openButton.addEventListener("click", () => {
+    menu.showModal();
+  });
+
+  closeButton.addEventListener("click", () => {
+    menu.close();
+  });
+};
